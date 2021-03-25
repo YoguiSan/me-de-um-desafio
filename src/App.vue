ab61a6cfc30bcaa7dc8a1b71b8e0be029a938fdc
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2021-03-25 10:38:49 -0300

    Ajustados JSON's, CSS e componente AssettoCorsa

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,54 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: #171711;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+
+a {
+  color: lightgreen;
+}
+
+button {
+  background: none;
+  border: solid 3px white;
+  border-radius: .5rem;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: .5rem;
+  transition: all .3s;
+}
+
+button:hover {
+  background: white;
+  color: #2c3e50;
+}
+
+#challenge-info-container {
+  border-top: solid 1px white;
+  margin: auto;
+  max-width: 40rem;
+}
+
+@media all and (max-width: 560px) {
+  #challenge-info-container {
+    max-width: 90%;
+  }
+}
+
 </style>
--- src/components/AssettoCorsa.vue
+++ src/components/AssettoCorsa.vue
@@ -2,17 +2,37 @@
   <article id="assetto-corsa">
     <h1>Me dê um desafio - Assetto Corsa</h1>
     <button
+      id="button-get-challenge"
       type="button"
       v-on:click="() => obterAleatorios()"
     >
-      Aleatório
+      {{ circuito && carro ? 'Outra vez' : 'Sortear' }}
     </button>
-    <section>
-      <h2>Circuito</h2>
-      <p>{{ circuito || 'Não selecionado' }}</p>
+    <section
+      id="challenge-info-container"
+      v-if="circuito && carro"
+    >
+      <figure>
+        <h2>Circuito</h2>
+        <img
+          :src="imgCircuito.length > 0
+            ? require(`../assets/img/circuitos/${imgCircuito}`)
+            : defaultImage
+          "
+        >
+        <p>{{ circuito || 'Não selecionado' }}</p>
+      </figure>
 
-      <h2>Carro</h2>
-      <p>{{ carro || 'Não selecionado' }}</p>
+      <figure>
+        <h2>Carro</h2>
+        <img
+          :src="imgCarro.length > 0
+            ? require(`../assets/img/carros/${imgCarro}`)
+            : defaultImage
+          "
+        >
+        <p>{{ carro || 'Não selecionado' }}</p>
+      </figure>
     </section>
   </article>
 </template>
@@ -26,12 +46,17 @@ import { obterElementoAleatorioArray } from 'formatadores';
 import ListaCircuitos from '../utils/json/lista-circuitos.json';
 import ListaCarros from '../utils/json/lista-carros.json';
 
+import DefaultImage from '../assets/img/No-image-found.jpg';
+
 export default {
   name: 'AssettoCorsa',
   data() {
     return {
       circuito: null,
       carro: null,
+      defaultImage: DefaultImage,
+      imgCircuito: null,
+      imgCarro: null,
     };
   },
   methods: {
@@ -42,10 +67,40 @@ export default {
       return obterElementoAleatorioArray(ListaCarros);
     },
     obterAleatorios: function obterAleatorios() {
-      this.circuito = this.obterCircuitoAleatorio();
-      this.carro = this.obterCarroAleatorio();
+      this.circuito = this.obterCircuitoAleatorio().nome;
+      this.carro = this.obterCarroAleatorio().nome;
+      this.imgCircuito = this.obterCircuitoAleatorio().urlImagem;
+      this.imgCarro = this.obterCarroAleatorio().urlImagem;
     },
   },
 };
 </script>
 
+<style>
+#button-get-challenge {
+  margin-bottom: 1rem;
+}
+
+#challenge-info-container {
+  display: flex;
+}
+
+figure {
+  width: 50%;
+}
+
+figure > img {
+  margin: auto;
+  width: 100%;
+}
+
+figure > p {
+  background: white;
+  color: black;
+  display: block;
+  margin: 0;
+  padding: 1rem;
+  width: 100%;
+}
+
+</style>
